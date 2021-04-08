c31da95d81aa0f076b9af8ba79cfdc0c39cd23ee
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-04-08 16:44:57 +0530

    code to pass floyd test added

--- server.go
+++ server.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	out := "20"
-	fmt.Fprintf(w, out)
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	out := ""
+	if player == "Floyd" {
+		out = "10"
+	}
+	if player == "Pepper" {
+		out = "20"
+	}
+	fmt.Fprint(w, out)
 }
