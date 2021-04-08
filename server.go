fd83fb390c69cfbc3d9f00dd047140bdb8d3b1f9
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-04-08 19:01:55 +0530

    server code made dynamic

--- server.go
+++ server.go
@@ -6,14 +6,35 @@ import (
 	"strings"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+	RecordWin(name string)
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	out := ""
-	if player == "Floyd" {
-		out = "10"
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
 	}
-	if player == "Pepper" {
-		out = "20"
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
 	}
-	fmt.Fprint(w, out)
+	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.store.RecordWin(player)
+	w.WriteHeader((http.StatusAccepted))
 }
