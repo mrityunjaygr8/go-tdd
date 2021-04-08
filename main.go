c4279e37306ece285da47511392669cc9668c3c4
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-04-08 16:44:37 +0530

    main file added

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Yo yoy o")
+	handler := http.HandlerFunc(PlayerServer)
+	log.Fatal(http.ListenAndServe(":5000", handler))
 }
