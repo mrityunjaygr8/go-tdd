49cc84dc40fd1d92c56ec48a30c231d78cceff7e
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-04-13 14:35:54 +0530

    integration test and main now uses league and fileSystemStore

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const dbFile = "game.db.json"
+
 func main() {
-	server := NewPlayerServer(NewInMemoryPlayerStore())
+	db, err := os.OpenFile(dbFile, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFile, err)
+	}
+
+	store := &FileSystemPlayerStore{db}
+	server := NewPlayerServer(store)
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
